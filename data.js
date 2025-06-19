e24d02d9ffdc4791d03b1d248e3769c69f8979a7
Viet-Tham Huynh <44217470+viettham1998@users.noreply.github.com>
2025-06-19 17:00:21 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,5 @@
 const conferences = [
-  { name: "CVPR 2025", rank: "A*", date: "2025-06-20", location: "Seattle, USA" },
-  { name: "ICCV 2025", rank: "A*", date: "2025-10-15", location: "Paris, France" },
-  { name: "KDD 2025", rank: "A",  date: "2025-08-12", location: "San Francisco, USA" },
-  { name: "VLDB 2024", rank: "A", date: "2024-05-05", location: "Munich, Germany" },
-  { name: "AAAI 2024", rank: "A*", date: "2024-02-01", location: "Vancouver, Canada" }
+  { name: "ISMAR 2025 (Poster)", rank: "A*", date: "2025-7-29", location: "Daejeon, South Korea" },
+  { name: "MMM 2026", rank: "B", date: "2025-08-9", location: "Prague, Czech Republic" },
+  { name: "VRST 2025", rank: "A",  date: "2025-07-07", location: "Montreal" }
 ];
